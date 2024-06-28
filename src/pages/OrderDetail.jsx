f5e2ad67bf84a467c6c0a434cb6f447671efdb97
poyu <chen06115@gmail.com>
2024-06-28 11:57:40 +0800

    新增購物車、訂單紀錄pages

---
+++ src/pages/OrderDetail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function OrderDetail(){
+    return(
+        <>
+            <h1>購物車</h1>
+        </>
+    )
+};
+
+export default OrderDetail;
---
+++ src/pages/Orders.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Orders(){
+    return(
+        <>
+            <h1>訂單紀錄</h1>
+        </>
+    )
+};
+
+export default Orders;
