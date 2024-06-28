eec10233b0b8cd271cbe00ae1bde30f2bd6b24ba
poyu <chen06115@gmail.com>
2024-06-28 11:38:05 +0800

    修改路由設定，將網頁導向至home(由於github pages是讀取index.html，但內容為空，所以導向至正確位址)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Contact from './pages/Contact';
 import Navbar from './components/Navbar';
 
 function App() {
-
   return (
     <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </Router>
-  )
+      <Navbar />
+      <Routes>
+        {/* 重定向 /OrderSystem 到 /home */}
+        <Route path="/OrderSystem" element={<Navigate to="/home" replace />} />
+        
+        {/* 定义主页和其他页面的路由 */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        
+        {/* 默认重定向 */}
+        <Route path="*" element={<Navigate to="/home" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
