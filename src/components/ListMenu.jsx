e46be8b7bea18206369eea702a74b1f23cc4e0ce
poyu <chen06115@gmail.com>
2024-06-30 01:37:33 +0800

    修改UI為card樣式

--- src/components/ListMenu.jsx
+++ src/components/ListMenu.jsx
@@ -14,20 +14,44 @@ function ListMenu({ items, onEdit, onDelete, userMode }) {
   return (
     <>
       {Object.keys(groupedItems).map(type => (
-        <div key={type} className="container">
-          <h2>{type}</h2>
-          <ul>
-            {groupedItems[type].map(menuItem => (
-              <li key={menuItem.id}>
-                {menuItem.name} - ${menuItem.price}
-                {userMode === 'Admin' && (
-                  <>
-                    <button onClick={() => onEdit(menuItem.id)}>編輯</button>
-                    <button onClick={() => onDelete(menuItem.id)}>刪除</button>
-                  </>)}
-              </li>
-            ))}
-          </ul>
+        <div key={type} className="row">
+          <h2 className='text-center'>{type}</h2>
+          <hr/>
+          {groupedItems[type].map(menuItem=>(
+            <div key={menuItem.id} className='col-lg-4 col-md-6 col-8 mx-md-0 mx-auto'>
+              <div className='card'>
+                <div className='card-body'>
+                  <h3 className='card-title text-center'>{menuItem.name}</h3>
+                  <div className='card-text'>
+                    <p>價格:{menuItem.price}</p>
+                    <form className='d-sm-flex justify-content-center text-center'>
+                      <div className='col-12 col-sm-6'>
+                        <select className='form-select'>
+                          <option>請選擇數量</option>
+                          <option value='1'>1</option>
+                          <option value='2'>2</option>
+                          <option value='3'>3</option>
+                          <option value='4'>4</option>
+                          <option value='5'>5</option>
+                          <option value='6'>6</option>
+                          <option value='7'>7</option>
+                          <option value='8'>8</option>
+                          <option value='9'>9</option>
+                          <option value='10'>10</option>
+                        </select>
+                      </div>
+                      <button className='btn btn-primary mt-1 mt-sm-0 ms-2'>加入購物車</button>
+                    </form>
+                    {userMode === 'Admin' && (
+                      <div className='container mt-1 d-flex justify-content-center'>
+                        <button className='me-1 btn btn-warning' onClick={() => onEdit(menuItem.id)}>編輯</button>
+                        <button className='me-1 btn btn-danger' onClick={() => onDelete(menuItem.id)}>刪除</button>
+                      </div>)}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       ))}
     </>
